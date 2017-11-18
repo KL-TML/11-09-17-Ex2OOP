139371a197450dc0f004c74dd0d9dd674176dbb3
KYLE LIPTON <lipton.kyle@gmail.com>
2017-11-17 20:20:01 -0500

    added parent method

--- people.rb
+++ people.rb
@@ -1,16 +1,32 @@
-class Student
+class Person
 
-def learn
-  return "I get it!"
+  def initialize(name)
+    @name = name
+  end
+
+  def greetings
+    puts "Hi, my name is #{name}"
+  end
+
 end
 
 
+
+class Student < Person
+
+  def learn
+    return "I get it!"
+  end
+
+
 end
 
-class Instructor
+class Instructor < Person
 
-def teach
-  return "Everything in Ruby is an Object"
-end
+  def teach
+    return "Everything in Ruby is an Object"
+  end
+
+
 
 end
